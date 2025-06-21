50a28a4a2449a8cd9774add99a4a7e1e31996c50
ShijiGovinP <shijigovin2004@gmail.com>
2025-06-21 11:55:10 +0530

    financialforecasting.java

--- financialforecasting.java
+++ financialforecasting.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class financial {
     public static double value(int year, double rate, double initial) {
         if (year == 0) {
@@ -7,10 +8,22 @@ public class financial {
         }
     }
     public static void main(String[] args) {
-        double initialInvestment = 10000; 
-        double growthRate = 0.07; 
-        int years = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the initial investment: ");
+        double initialInvestment = sc.nextDouble();
+        System.out.print("Enter the annual growth rate : ");
+        double growthRate = sc.nextDouble();
+        System.out.print("Enter the number of years: ");
+        int years = sc.nextInt();
         double futureValue = value(years, growthRate, initialInvestment);
-        System.out.println("recursive" + futureValue);
+        System.out.println("Future value after " + years + " years is " + futureValue);
     }
 }
+
+
+// OUTPUT:
+
+// Enter the initial investment: 3445
+// Enter the annual growth rate : 45.6
+// Enter the number of years: 6
+// Future value after 6 years is 3.527806258744418E13
