956597bd56774380540a3def4f1cc45919edb766
ShijiGovinP <shijigovin2004@gmail.com>
2025-06-21 22:17:04 +0530

    financial.java

--- Week 1 DataStructuresAndAlgorithms/financial.java
+++ Week 1 DataStructuresAndAlgorithms/financial.java
@@ -1,30 +1,4 @@
-// public class financial {
-//     public static double value(int year, double rate, double initial) {
-//         if (year == 0) {
-//             return initial;
-//         } else {
-//             return value(year - 1, rate, initial) * (1 + rate);
-//         }
-//     }
-   
-//     public static void main(String[] args) {
-//         double initialInvestment = 10000; 
-//         double growthRate = 0.07; 
-//         int years = 10;
-//         double futureValue = value(years, growthRate, initialInvestment);
-//         System.out.println("recursive" + futureValue);
-//     }
-// }
-
-
-
-
-
-
-
-
 import java.util.Scanner;
-
 public class financial {
     public static double value(int year, double rate, double initial) {
         if (year == 0) {
@@ -33,7 +7,6 @@ public class financial {
             return value(year - 1, rate, initial) * (1 + rate);
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the initial investment: ");
@@ -46,6 +19,13 @@ public class financial {
         System.out.println("Future value after " + years + " years is " + futureValue);
     }
 }
+
+
+// OUTPUT
+// Enter the initial investment: 25000
+// Enter the annual growth rate : 56.6
+// Enter the number of years: 5
+// Future value after 5 years is 1.5850845241344E13
 
 
     
