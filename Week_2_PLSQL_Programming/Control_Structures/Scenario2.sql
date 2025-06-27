e26c74e9ce6c346ccde8ac08cc55d68aca35e669
ShijiGovinP <shijigovin2004@gmail.com>
2025-06-27 21:13:57 +0530

    Scenario2.sql

---
+++ Week_2_PLSQL_Programming/Control_Structures/Scenario2.sql
@@ -0,0 +1,12 @@
+BEGIN
+DBMS_OUTPUT.PUT_LINE(CHR(10) || 'VIP Customers (Balance > 10,000));
+    FOR cust IN (
+        SELECT CustomerID, Name, Balance FROM Customers WHERE Balance > 10000
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE('CustomerID: ' || cust.CustomerID || 
+                             ', Name: ' || cust.Name || 
+                             ', Balance: ' || cust.Balance);
+    END LOOP;
+COMMIT;
+END;
+/   
