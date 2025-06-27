442ee2b72e83087e8b8324204eaaa4685579755f
ShijiGovinP <shijigovin2004@gmail.com>
2025-06-27 21:45:57 +0530

    Scenario1.sql

--- Week_2_PLSQL_Programming/Stored_Procedures/Scenario1.sql
+++ Week_2_PLSQL_Programming/Stored_Procedures/Scenario1.sql
@@ -1,1 +1,13 @@
+-- Procedure 1: ProcessMonthlyInterest
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
+BEGIN
+    UPDATE Accounts
+    SET Balance = Balance * 1.01
+    WHERE UPPER(AccountType) = 'SAVINGS';
 
+    DBMS_OUTPUT.PUT_LINE('1% interest applied to all savings accounts.');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error in interest processing: ' || SQLERRM);
+END;
+/
