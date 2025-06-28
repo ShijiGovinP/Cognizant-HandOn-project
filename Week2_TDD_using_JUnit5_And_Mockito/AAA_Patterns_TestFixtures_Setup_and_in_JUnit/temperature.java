6a6cfc34de4654444c7f63954dfe76b3e3a1eaf9
ShijiGovinP <shijigovin2004@gmail.com>
2025-06-28 14:33:34 +0530

    temperature.java

---
+++ Week2_TDD_using_JUnit5_And_Mockito/AAA_Patterns_TestFixtures_Setup_and_in_JUnit/temperature.java
@@ -0,0 +1,23 @@
+ppublic class TemperatureConverter {
+
+    public double celsiustoFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public double fahrenheittoCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public double roundTwoDecimals(double value) {
+        double shifted = value * 100;
+        double fractionalPart = shifted - (int)shifted;
+        int rounded;
+        if (fractionalPart >= 0.5) {
+            rounded = (int)shifted + 1;
+        } else {
+            rounded = (int)shifted;
+        }
+
+        return rounded / 100.0;
+    }
+}
