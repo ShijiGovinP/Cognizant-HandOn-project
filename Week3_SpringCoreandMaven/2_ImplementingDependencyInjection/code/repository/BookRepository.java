883f601ed309341ab815ea8936103d1e26c8b24d
ShijiGovinP <shijigovin2004@gmail.com>
2025-07-04 21:09:19 +0530

    BookRepository.java

---
+++ Week3_SpringCoreandMaven/2_ImplementingDependencyInjection/code/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void saveBook(String bookName) {
+        System.out.println("Book '" + bookName + "' saved to database.");
+    }
+
+    public void addBook(String lord_of_the_files) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
