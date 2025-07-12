a45c0990c74083a55388dd5ed7e0e57eb45d3b39
ShijiGovinP <shijigovin2004@gmail.com>
2025-07-12 15:07:26 +0530

    JwtUtil.java

---
+++ Week4_Spring_REST_using_Spring_Boot3/5_JWT-handson/1_Create_authentication_service_that_returns_JWT/src/main/java/com/example/jwtauth/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.cognizant.jwt_auth.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "secret123"; // Store securely in real apps
+    private final long EXPIRATION_TIME = 60 * 60 * 1000; // 1 hour in milliseconds
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
