463fbb735a9d8181425bf6662d9a20a644f1afb1
ShijiGovinP <shijigovin2004@gmail.com>
2025-07-12 15:08:09 +0530

    SecurityConfig.java

---
+++ Week4_Spring_REST_using_Spring_Boot3/5_JWT-handson/1_Create_authentication_service_that_returns_JWT/src/main/java/com/example/jwtauth/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cognizant.jwt_auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults())
+            .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
